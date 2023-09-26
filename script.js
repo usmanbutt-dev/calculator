d95521c025ee2c3111ba675321cbcda8b4230b90
Usman <buttu7666@gmail.com>
2023-09-26 06:10:24 +0500

    Event listeners added to keyboard strokes and clicks

---
+++ script.js
@@ -0,0 +1,28 @@
+let numbers = document.querySelectorAll(".number");
+let operators = document.querySelectorAll(".btn"); 
+let input = document.querySelector(".second-line");
+let erase = document.querySelector("#erase")
+
+function deleteOne() {
+    input.innerHTML = input.innerHTML.slice(0, -1); 
+    console.log("Works")
+}
+
+document.addEventListener("keydown", (e)=> {
+    if(e.key >= '0' && e.key <= '9') {
+        input = document.querySelector(".second-line");
+        input.innerHTML += e.key;
+    }
+    else if(e.key ==="Backspace") {
+        deleteOne();
+    }
+
+})
+
+numbers.forEach(number => {
+    number.addEventListener("click", () => {
+        input.innerHTML += number.innerHTML;
+    })
+});
+
+erase.addEventListener("click", deleteOne)
